d92bc1aaf28457e3299b78a62871a36f4fa8e0d3
Bartłomiej Staroń <bstaron@Gispartner.pl>
2019-07-17 23:22:27 +0200

    1:17:53

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -76,6 +76,16 @@ app.post('/scream', (req, res) => {
 
 })
 
+const isEmpty = (string) => {
+    if (string.trim() === '') return true;
+    else return false;
+}
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(regEx)) return true;
+    else return false;
+};
+
 app.post('/signup', (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -83,6 +93,26 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle,
     };
+
+    let errors = {};
+
+    if (isEmpty(newUser.email)) {
+        error.email = 'Must not be empty'
+
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address'
+    }
+
+    if (isEmpty(newUser.email))
+        error.email = 'Must not be empty'
+
+    if (newUser.password !== newUser.confirmPassword) errors.password = 'Password must match'
+
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+    if (Object.keys(errors).length > 0) {
+        return res.status(400).json(errors);
+    }
     let token, userId;
 
     db.doc(`/users/${newUser.handle}`).get()
@@ -117,6 +147,29 @@ app.post('/signup', (req, res) => {
             }
         })
 
+})
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if (isEmpty(user.email)) errors.email = 'Must not be empty';
+    if (isEmpty(user.password)) errors.password = 'Must not be empty';
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password).then(data => {
+        return data.user.getIdToken();
+    }).then(token => {
+        return res.json({token});
+    }).catch(err =>{
+        console.error(err)
+        return res.status(500).json({ errors: err.code});
+    });
 })
 
 exports.api = functions.region('europe-west1').https.onRequest(app)
